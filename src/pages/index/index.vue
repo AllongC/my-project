b0d1dfeec5fd04337ab6eb48e05b42588f838360
杨腾祺 <3150475761@qq.com>
2020-08-09 19:25:42 +0800

    导航条

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,11 +7,18 @@
     <!-- 2.0 轮播图 start-->
     <swiper autoplay circular indicator-dots>
       <swiper-item v-for="item in bananers" :key="item.goods_id">
-        <image :src="item.image_src" mode="widthFix"></image>
+        <image :src="item.image_src" mode="widthFix" />
       </swiper-item>
     </swiper>
     <!-- 2.0 轮播图 end-->
 
+    <!-- 3.0 导航 start -->
+    <view class="nav">
+      <navigator v-for="item in navList" :key="item.goods_id">
+        <image :src="item.image_src" mode="widthFix" />
+      </navigator>
+    </view>
+    <!-- 3.0 导航 end -->
   </view>
 </template>
 
@@ -24,10 +31,12 @@ export default {
   data() {
     return {
       bananers: [],
+      navList: []
     };
   },
   onLoad() {
     this.getBananer();
+    this.getNav();
   },
   methods: {
     getBananer() {
@@ -38,13 +47,31 @@ export default {
         .then(res => {
           this.bananers = res[1].data.message;
         });
+    },
+    getNav() {
+      uni
+        .request({
+          url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
+        })
+        .then(res => {
+          this.navList = res[1].data.message;
+        });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-swiper{
+swiper {
   height: 340rpx;
+}
+.nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  navigator {
+    flex: 1;
+    padding: 30rpx;
+  }
 }
 </style>
