717813d2979a7c114bd1ee9da66e101dee5c39d2
杨腾祺 <3150475761@qq.com>
2020-08-10 17:49:54 +0800

    分类页面渲染

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -7,208 +7,27 @@
     <!-- 2.0 分类内容 start-->
     <view class="main">
       <view class="main-left">
-        <view class="item active">1</view>
-        <view class="item">2</view>
-        <view class="item">3</view>
-        <view class="item">4</view>
-        <view class="item">5</view>
-        <view class="item">6</view>
-        <view class="item">7</view>
-        <view class="item">8</view>
-        <view class="item">9</view>
-        <view class="item">10</view>
-        <view class="item">11</view>
-        <view class="item">12</view>
-        <view class="item">13</view>
-        <view class="item">14</view>
-        <view class="item">15</view>
-        <view class="item">16</view>
-        <view class="item">17</view>
-        <view class="item">18</view>
-        <view class="item">19</view>
-        <view class="item">20</view>
-        <view class="item">21</view>
-        <view class="item">22</view>
-        <view class="item">23</view>
-        <view class="item">24</view>
-        <view class="item">25</view>
-        <view class="item">26</view>
-        <view class="item">27</view>
-        <view class="item">28</view>
-        <view class="item">29</view>
-        <view class="item">30</view>
-        <view class="item">31</view>
-        <view class="item">32</view>
-        <view class="item">33</view>
-        <view class="item">34</view>
-        <view class="item">35</view>
-        <view class="item">36</view>
-        <view class="item">37</view>
-        <view class="item">38</view>
-        <view class="item">39</view>
-        <view class="item">40</view>
-        <view class="item">41</view>
-        <view class="item">42</view>
-        <view class="item">43</view>
-        <view class="item">44</view>
-        <view class="item">45</view>
-        <view class="item">46</view>
-        <view class="item">47</view>
-        <view class="item">48</view>
-        <view class="item">49</view>
-        <view class="item">50</view>
-        <view class="item">51</view>
-        <view class="item">52</view>
-        <view class="item">53</view>
-        <view class="item">54</view>
-        <view class="item">55</view>
-        <view class="item">56</view>
-        <view class="item">57</view>
-        <view class="item">58</view>
-        <view class="item">59</view>
-        <view class="item">60</view>
-        <view class="item">61</view>
-        <view class="item">62</view>
-        <view class="item">63</view>
-        <view class="item">64</view>
-        <view class="item">65</view>
-        <view class="item">66</view>
-        <view class="item">67</view>
-        <view class="item">68</view>
-        <view class="item">69</view>
-        <view class="item">70</view>
-        <view class="item">71</view>
-        <view class="item">72</view>
-        <view class="item">73</view>
-        <view class="item">74</view>
-        <view class="item">75</view>
-        <view class="item">76</view>
-        <view class="item">77</view>
-        <view class="item">78</view>
-        <view class="item">79</view>
-        <view class="item">80</view>
-        <view class="item">81</view>
-        <view class="item">82</view>
-        <view class="item">83</view>
-        <view class="item">84</view>
-        <view class="item">85</view>
-        <view class="item">86</view>
-        <view class="item">87</view>
-        <view class="item">88</view>
-        <view class="item">89</view>
-        <view class="item">90</view>
-        <view class="item">91</view>
-        <view class="item">92</view>
-        <view class="item">93</view>
-        <view class="item">94</view>
-        <view class="item">95</view>
-        <view class="item">96</view>
-        <view class="item">97</view>
-        <view class="item">98</view>
-        <view class="item">99</view>
-        <view class="item">100</view>
+        <view
+          v-for="(item,index) in titleList"
+          :key="index"
+          :class="['item',currentId==index?'active':'']"
+          @click="chageCurrent(index)"
+        >{{item}}</view>
       </view>
       <view class="main-right">
-        <view class="product">1</view>
-        <view class="product">2</view>
-        <view class="product">3</view>
-        <view class="product">4</view>
-        <view class="product">5</view>
-        <view class="product">6</view>
-        <view class="product">7</view>
-        <view class="product">8</view>
-        <view class="product">9</view>
-        <view class="product">10</view>
-        <view class="product">11</view>
-        <view class="product">12</view>
-        <view class="product">13</view>
-        <view class="product">14</view>
-        <view class="product">15</view>
-        <view class="product">16</view>
-        <view class="product">17</view>
-        <view class="product">18</view>
-        <view class="product">19</view>
-        <view class="product">20</view>
-        <view class="product">21</view>
-        <view class="product">22</view>
-        <view class="product">23</view>
-        <view class="product">24</view>
-        <view class="product">25</view>
-        <view class="product">26</view>
-        <view class="product">27</view>
-        <view class="product">28</view>
-        <view class="product">29</view>
-        <view class="product">30</view>
-        <view class="product">31</view>
-        <view class="product">32</view>
-        <view class="product">33</view>
-        <view class="product">34</view>
-        <view class="product">35</view>
-        <view class="product">36</view>
-        <view class="product">37</view>
-        <view class="product">38</view>
-        <view class="product">39</view>
-        <view class="product">40</view>
-        <view class="product">41</view>
-        <view class="product">42</view>
-        <view class="product">43</view>
-        <view class="product">44</view>
-        <view class="product">45</view>
-        <view class="product">46</view>
-        <view class="product">47</view>
-        <view class="product">48</view>
-        <view class="product">49</view>
-        <view class="product">50</view>
-        <view class="product">51</view>
-        <view class="product">52</view>
-        <view class="product">53</view>
-        <view class="product">54</view>
-        <view class="product">55</view>
-        <view class="product">56</view>
-        <view class="product">57</view>
-        <view class="product">58</view>
-        <view class="product">59</view>
-        <view class="product">60</view>
-        <view class="product">61</view>
-        <view class="product">62</view>
-        <view class="product">63</view>
-        <view class="product">64</view>
-        <view class="product">65</view>
-        <view class="product">66</view>
-        <view class="product">67</view>
-        <view class="product">68</view>
-        <view class="product">69</view>
-        <view class="product">70</view>
-        <view class="product">71</view>
-        <view class="product">72</view>
-        <view class="product">73</view>
-        <view class="product">74</view>
-        <view class="product">75</view>
-        <view class="product">76</view>
-        <view class="product">77</view>
-        <view class="product">78</view>
-        <view class="product">79</view>
-        <view class="product">80</view>
-        <view class="product">81</view>
-        <view class="product">82</view>
-        <view class="product">83</view>
-        <view class="product">84</view>
-        <view class="product">85</view>
-        <view class="product">86</view>
-        <view class="product">87</view>
-        <view class="product">88</view>
-        <view class="product">89</view>
-        <view class="product">90</view>
-        <view class="product">91</view>
-        <view class="product">92</view>
-        <view class="product">93</view>
-        <view class="product">94</view>
-        <view class="product">95</view>
-        <view class="product">96</view>
-        <view class="product">97</view>
-        <view class="product">98</view>
-        <view class="product">99</view>
-        <view class="product">100</view>
+        <view class="product" v-for="(item,index) in productList" :key="index">
+          <view class="product-title">
+            <text>/</text>
+            <text>{{item.cat_name}}</text>
+            <text>/</text>
+          </view>
+          <view class="product-all">
+            <view class="product-shop" v-for="(item,index) in item.children" :key="index">
+              <image :src="item.cat_icon" mode="widthFix"></image>
+              <view>{{item.cat_name}}</view>
+            </view>
+          </view>
+        </view>
       </view>
     </view>
     <!-- 2.0 分类内容 end-->
@@ -217,9 +36,36 @@
 
 <script>
 import SearchInput from "@/components/searchInput";
+
+let dataAll = [];
+
 export default {
   components: {
     SearchInput
+  },
+  data() {
+    return {
+      titleList: [],
+      productList: [],
+      currentId: 0
+    };
+  },
+  onLoad() {
+    uni
+      .request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+      })
+      .then(res => {
+        dataAll = res[1].data.message;
+        this.titleList = dataAll.map(item => item.cat_name);
+        this.productList = dataAll[0].children;
+      });
+  },
+  methods: {
+    chageCurrent(index) {
+      this.currentId = index;
+      this.productList = dataAll[index].children;
+    }
   }
 };
 </script>
@@ -249,6 +95,26 @@ export default {
       height: 100%;
       flex: 1;
       overflow: auto;
+      .product-title {
+        line-height: 100rpx;
+        text-align: center;
+        text:nth-child(2) {
+          margin: 0rpx 10rpx;
+        }
+      }
+      .product-all{
+        display: flex;
+        flex-wrap: wrap;
+      }
+      .product-shop{
+        width: 33.33%;
+        text-align: center;
+        padding: 5rpx;
+        image{
+          width: 104rpx;
+          height: 106rpx;
+        }
+      }
     }
   }
 }
